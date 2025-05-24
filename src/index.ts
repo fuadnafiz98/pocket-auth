cedd25ebcf928e6a6356783dc7530a4d2338354e
fuadnafiz98 <fuadnafiz98@gmail.com>
2025-05-24 21:58:43 +0600

    impv: callback URLs and getting data from google

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,29 @@ import { object, string } from 'valibot';
 import { GoogleProvider } from '@openauthjs/openauth/provider/google';
 import { CloudflareStorage } from '@openauthjs/openauth/storage/cloudflare';
 
+import { createClient } from '@openauthjs/openauth/client';
+
+const client = createClient({
+	clientID: 'fuadnafiz98.com',
+	//TODO: remove hardcoded urls
+	issuer: 'http://localhost:3000',
+});
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		if (url.pathname === '/callback') {
+			const url = new URL(request.url);
+			const code = url.searchParams.get('code');
+
+			//TODO: remove hardcoded urls
+			const exchanged = await client.exchange(code!, `http://localhost:3000/callback`);
+
+			console.log(exchanged);
+
+			return new Response('HI');
+		}
+
 		const app = issuer({
 			providers: {
 				google: GoogleProvider({
@@ -20,10 +41,25 @@ export default {
 			subjects: {
 				user: object({
 					email: string(),
+					// TODO: add more info here
+					// picture: string(),
+					// name: string(),
 				}),
 			},
-			success: async (response, input, req) => {
-				console.log(response, input, req);
+			success: async (ctx, value) => {
+				if (value.provider === 'google') {
+					const response = await fetch('https://openidconnect.googleapis.com/v1/userinfo', {
+						headers: {
+							Authorization: `Bearer ${value.tokenset.access}`,
+						},
+					});
+					const data: any = await response.json();
+					// TODO: create the user here
+
+					return ctx.subject('user', {
+						email: data.email,
+					});
+				}
 				return new Response('success');
 			},
 		});
